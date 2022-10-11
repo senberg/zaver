a0500fa405e1d9fa7f33fc360bc3fe9a812d523e
Staffan Enberg <senberg@live.se>
2022-10-11 20:45:25 +0200

    first commit

---
+++ src/main/java/se/senberg/zaver/ZaverApplication.java
@@ -0,0 +1,13 @@
+package se.senberg.zaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaverApplication.class, args);
+	}
+
+}
